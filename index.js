f58a6d3a6ba9bc4b18e2b8c3dd66d6afdcef0139
Jade Allen Cook <jadeallencook@gmail.com>
2020-06-06 11:24:30 -0700

    Added boilerplate code

--- index.js
+++ index.js
@@ -1,23 +1,19 @@
 #!/usr/bin/env node
 
+const messages = require('./messages');
 const { argv } = process;
+const length = argv.length;
 
 class Cookup {
-    constructor() {
-        this.filename = '';
-        this.extension = '';
-        this.parameters = [];
-    }
-    store() {
-
-    }
-    generate() {
-
-    }
+  constructor() {
+    const name = argv[2].split('.');
+    this.filename = name.length >= 2 ? name[0] : null;
+    this.extension = name.length >= 2 ? name[name.length - 1] : null;
+    this.parameters = length > 3 ? argv.splice(3, length) : null;
+    this.command = !this.filename ? name[0] : null;
+  }
+  store() {}
+  generate() {}
 }
 
-if (argv.length < 3) {
-  console.log('NO PARAMS');
-} else {
-  console.log(new Cookup(argv));
-}
+console.log(length < 3 ? messages.default : new Cookup());
---
+++ messages.js
@@ -0,0 +1,2 @@
+exports.default =
+  '\n\x1b[34mcookup 2.0.0\x1b[0m by @jadeallencook\n\x1b[33mcreate cookup:\x1b[0m cookup filename.ext command\n\x1b[33muse cookup:\x1b[0m cookup command filename\n';
